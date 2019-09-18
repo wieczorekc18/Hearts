7012d3a79e8619000112fcb69a69dff82edb3aee
wieczorekc18 <wieczorekc18@gmail.com>
2019-09-18 10:53:39 -0400

    Change score

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ const rulesText = document.querySelector(".rules-text")
 const rulesX = document.querySelector(".rules-x")
 const scoreButton = document.querySelector(".score-button")
 
-const scoreX = document.querySelector(".score-x")
+// const scoreX = document.querySelector(".score-x")
 
-// scoreButton.addEventListener("click", ()=>{
-//     score.classList.toggle("hide-score")
-// })
+scoreButton.addEventListener("click", ()=>{
+    score.classList.toggle("hide-score")
+})
 
 // scoreX.addEventListener("click", ()=>{
 //     score.classList.add("hide-score")
